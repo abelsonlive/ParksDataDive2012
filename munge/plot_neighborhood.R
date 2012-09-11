a2876e26d8d2ab73f3f54aa7203b8dc0bd46c03b
Adam Laiacano <adam@tumblr.com>
2012-09-10 21:43:21 -0400

    now have lat/lon instead of x/y

--- munge/plot_neighborhood.R
+++ munge/plot_neighborhood.R
@@ -16,9 +16,10 @@ qplot(x, y, data=street.trees[sample(1:nrow(street.trees), 10000),])
 
 d <- grabxy(street.trees, lon, lat, range=.03)
 
-d2 <- ddply(idata.frame(subset(d, species != "other")), .(season, species), summarize, tot=length(season))
+d2 <- ddply(idata.frame(d), .(season, species), summarize, tot=length(season))
 d3 <- ddply(idata.frame(d2), .(species), function(x) {data.frame(season=x$season, tot.trees=cumsum(x$tot))})
 
-qplot(x,y, data=d, color=log10(dbh)) + opts(title="Tree Diameters") + coord_map()
+qplot(lon,lat, data=d, color=log10(dbh)) + opts(title="Tree Diameters") + coord_map()
 qplot(as.numeric(season), tot.trees, data=d3, color=species, geom=c('point', 'line')) + opts(title="Top 10 Species")
 qplot(species, data=subset(d, species %in% names(summary(factor(d$species)))[1:10])) + opts(title="Species Diversity")
+
