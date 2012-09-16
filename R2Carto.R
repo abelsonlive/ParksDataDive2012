65cc2bb1e0d6f09a7f508527c194f9eac6901e59
Brian Abelson <brianabelson@gmail.com>
2012-09-15 21:10:32 -0300

    Update R2Carto.R

--- R2Carto.R
+++ R2Carto.R
@@ -2,6 +2,6 @@ path_to_file = "~/Desktop/Vizzuality-cartodb-r-05eb537/CartoDB_1.4.tar.gz"
 install.packages(path_to_file, repos=NULL, type="source")
 require("CartoDB")
 account_name = "parks-datadive"
-api_key = "4bcaf7ac1d56757cd065732b59b854f139d9ea67"
+api_key = "[key]"
 cartodb(account_name, api_key)
 cartodb.test()
